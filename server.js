e048c6ab44c700e16536455694e32ca2db3daa22
samarth <samarth.gugnani30@gmail.com>
2020-05-04 23:39:26 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var connectDB = require("./config/db");
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contracts", require("./routes/contracts"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("the server is on the port ", PORT);
+});
