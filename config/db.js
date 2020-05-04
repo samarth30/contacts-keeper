c137e8224a08a0c9864483a36b8d4cce6b7211de
samarth <samarth.gugnani30@gmail.com>
2020-05-04 23:40:22 +0530

    config/

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/contract-keeper", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => {
+      console.error(err.message);
+      process.exit(1);
+    });
+};
+
+module.exports = connectDB;
