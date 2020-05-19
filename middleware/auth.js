b6be33d31f0ef5296fa59f16e3ffbd53a02b59fd
samarth <samarth.gugnani30@gmail.com>
2020-05-19 21:25:58 +0530

    added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
 
+  // Check if not token
   if (!token) {
-    return res.status(401).send({ msg: "no token authorization" });
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token, "secret");
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
     req.user = decoded.user;
     next();
-  } catch (e) {
-    res.status(401).send({ msg: "token is not valid" });
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
